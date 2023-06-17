7aaa2d5e37e755faecef56e18b9796ca8b04e9cd
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-06-17 10:02:23 +0530

    Time: 24 ms (67.68%), Space: 44.4 MB (17.18%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,7 +1,17 @@
 class Solution {
     public boolean repeatedSubstringPattern(String str) {
-        String s = str + str;
-        System.out.println(s.substring(1,s.length()-1));
-        return s.substring(1, s.length() - 1).contains(str);
+        int l = str.length();
+	for(int i=l/2;i>=1;i--) {
+		if(l%i==0) {
+			int m = l/i;
+			String subS = str.substring(0,i);
+			StringBuilder sb = new StringBuilder();
+			for(int j=0;j<m;j++) {
+				sb.append(subS);
+			}
+			if(sb.toString().equals(str)) return true;
+		}
+	}
+	return false;
     }
 }
