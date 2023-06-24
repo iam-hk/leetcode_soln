0850bc57d1b1c81cc27b787dd79961d0a0739d36
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-06-24 09:45:45 +0530

    Time: 9 ms (79.82%), Space: 44.2 MB (66.39%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        HashSet<Character>hs=new HashSet<>();
+        for(int i=0;i<s.length();i++)
+            hs.add(s.charAt(i));
+        return hs.size();
+    }
+}
