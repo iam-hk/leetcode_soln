95320e2f816cc9ffe6181c7ab2cef42cf0399874
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-07-10 17:35:42 +0530

    Time: 13 ms (33.08%), Space: 62.4 MB (45.49%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,27 +14,24 @@
  * }
  */
 class Solution {
+    int min=Integer.MAX_VALUE;
+    public void fnDFS(TreeNode root,int depth)
+    {
+        if(root==null)
+            return;
+        if(root.left==null&&root.right==null)
+        {
+            min=Math.min(min,depth);
+            return;
+                
+        }
+        fnDFS(root.left,depth+1);
+        fnDFS(root.right,depth+1);
+    }
     public int minDepth(TreeNode root) {
         if(root==null)
             return 0;
-        Queue<TreeNode>q=new ArrayDeque<>();
-        q.add(root);
-        int depth=1;
-        while(q.size()!=0)
-        {
-            int size=q.size();
-            for(int i=0;i<size;i++)
-            {
-                TreeNode tmp=q.remove();
-                if(tmp.left==null&&tmp.right==null)
-                    return depth;
-                if(tmp.left!=null)
-                    q.add(tmp.left);
-                if(tmp.right!=null)
-                    q.add(tmp.right);
-            }
-            ++depth;
-        }
-        return 0;
+      fnDFS(root,1);
+        return min;
     }
 }
