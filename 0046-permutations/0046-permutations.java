10c83d96a21d196ab04c396b24dcfe7b76489709
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-07-13 09:36:26 +0530

    Time: 1 ms (98.92%), Space: 44.1 MB (28.74%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,30 @@
 class Solution {
-    List<List<Integer>>ans=new ArrayList<>();
-    public void fnPermute(int nums[],boolean visited[],List<Integer>a)
+    public void fnPermute(int []arr,boolean visited[],List<List<Integer>>ans,List<Integer>a)
     {
-        if(a.size()==nums.length)
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(a.size()==arr.length)
             {
                 ans.add(new ArrayList<>(a));
-                return ;
+                return;
             }
-        
-        for(int i=0;i<nums.length;i++)
-        {
-            
             if(!visited[i])
             {
-                a.add(nums[i]);
                 visited[i]=true;
-                fnPermute(nums,visited,a);
+                a.add(arr[i]);
+                fnPermute(arr,visited,ans,a);
                 visited[i]=false;
                 a.remove(a.size()-1);
+                
             }
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         boolean visited[]=new boolean[nums.length];
         List<Integer>a=new ArrayList<>();
-        
-        fnPermute(nums,visited,a);
+        List<List<Integer>>ans=new ArrayList<>();
+        fnPermute(nums,visited,ans,a);
         return ans;
     }
 }
