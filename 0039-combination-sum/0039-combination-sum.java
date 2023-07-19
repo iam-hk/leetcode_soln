dd8cf6343bf92793d75f2aee6aafe4ba98fa4a40
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-07-19 15:25:52 +0530

    Time: 3 ms (39.95%), Space: 43.7 MB (87.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,23 +1,25 @@
 class Solution {
-    public void fnComb(int arr[],int ind,List<List<Integer>>ans,List<Integer>a,int target)
+    public void fnComb(int arr[],int tar,List<List<Integer>>ans,List<Integer>a,int index)
     {
-        if(target==0)
+        if(tar==0)
         {
             ans.add(new ArrayList<>(a));
             return ;
+        }
+        if(index==arr.length||tar<0)
+        {
+            return ;
             
         }
-        if(ind==arr.length||target<0)
-            return;
-        a.add(arr[ind]);
-        fnComb(arr,ind,ans,a,target-arr[ind]);
+        a.add(arr[index]);
+        fnComb(arr,tar-arr[index],ans,a,index);
         a.remove(a.size()-1);
-        fnComb(arr,ind+1,ans,a,target);
+        fnComb(arr,tar,ans,a,index+1);
     }
-    public List<List<Integer>> combinationSum(int[] arr, int target) {
+    public List<List<Integer>> combinationSum(int[] arr, int tar) {
         List<List<Integer>>ans=new ArrayList<>();
         List<Integer>a=new ArrayList<>();
-        fnComb(arr,0,ans,a,target);
+        fnComb(arr,tar,ans,a,0);
         return ans;
     }
 }
