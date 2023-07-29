ec5835d60e2fd490dfe338d25f20dc8624632c0a
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-07-29 12:21:52 +0530

    Time: 9 ms (31.67%), Space: 41 MB (51.67%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.java
+++ 0808-soup-servings/0808-soup-servings.java
@@ -1,19 +1,17 @@
 class Solution {
     public double fnCalc(int a,int b,Map<String,Double>map)
     {
-        
         if(a<=0&&b<=0)
             return 0.5;
         if(a<=0)
             return 1;
-        else if(b<=0)
+        if(b<=0)
             return 0;
-        String st=a+" "+b;
-        if(map.containsKey(st))
-            return map.get(st);
-        
-        double val= 0.25*(fnCalc(a-100,b,map)+fnCalc(a-75,b-25,map)+fnCalc(a-50,b-50,map)+fnCalc(a-25,b-75,map));
-        map.put(st,val);
+        String tmp=a+" "+b;
+        if(map.containsKey(tmp))
+            return map.get(tmp);
+        double val=0.25*(fnCalc(a-100,b,map)+fnCalc(a-75,b-25,map)+fnCalc(a-50,b-50,map)+fnCalc(a-25,b-75,map)); 
+        map.put(tmp,val);
         return val;
     }
     public double soupServings(int n) {
