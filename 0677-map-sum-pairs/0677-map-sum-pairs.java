1a89cbc07d7db420bd2ae039b506b0a976d9545a
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-07-31 01:01:52 +0530

    Time: 12 ms (57.78%), Space: 42 MB (16.20%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.java
+++ 0677-map-sum-pairs/0677-map-sum-pairs.java
@@ -1,21 +1,48 @@
 class MapSum {
-    Map<String,Integer>hm;
+    class Node {
+        Node childs[]=new Node[26];
+        int count=0;
+    }
+    Map<String,Integer> hm;
+    Node head;
     public MapSum() {
         hm=new HashMap<>();
+        head=new Node();
     }
     
     public void insert(String key, int val) {
+        int diff=0;
+        Node root=head;
+        if(hm.containsKey(key))
+        {
+            diff=val-hm.get(key);
+               
+        }
         hm.put(key,val);
+        for(int i=0;i<key.length();i++)
+        {
+            char ch=key.charAt(i);
+            if(root.childs[ch-'a']==null)
+                root.childs[ch-'a']=new Node();
+            root=root.childs[ch-'a'];
+            if(diff!=0)
+            root.count+=diff;
+            else 
+                root.count+=val;
+        }
     }
     
     public int sum(String prefix) {
-        int count=0;
-        for(String s:hm.keySet())
+        Node root=head;
+        for(int i=0;i<prefix.length();i++)
         {
-            if(s.startsWith(prefix))
-                count+=hm.get(s);
+            char ch=prefix.charAt(i);
+            if(root.childs[ch-'a']==null)
+                return 0;
+            root=root.childs[ch-'a'];
+            
         }
-        return count;
+        return root.count;
     }
 }
 
