5864649b5117aba208bc89d49b404f15b5e1ef48
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-08-01 23:31:59 +0530

    Time: 18 ms (70.85%), Space: 45.2 MB (84.88%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,21 +1,23 @@
 class Solution {
     public void fnComb(int n,int k,List<List<Integer>>ans,List<Integer>a,boolean visited[],int val)
     {
-        if(a.size()==k)
-        {
-            ans.add(new ArrayList<>(a));
-            return ;
-        }
+       if(a.size()==k)
+       {
+           ans.add(new ArrayList<>(a));
+           return;
+           
+       }
         for(int i=val;i<=n;i++)
         {
-            if(!visited[i])
+            if(visited[i]==false)
             {
-            a.add(i);
+                 a.add(i);
                 visited[i]=true;
-            fnComb(n,k,ans,a,visited,i+1);
-            a.remove(a.size()-1);
+                fnComb(n,k,ans,a,visited,i+1);
+                a.remove(a.size()-1);
                 visited[i]=false;
             }
+           
         }
     }
     public List<List<Integer>> combine(int n, int k) {
