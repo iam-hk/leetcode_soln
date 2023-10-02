b605d9ea856d74c19f67c54bd2259a74a0016d54
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-10-02 19:08:51 +0530

    Added solution - LeetHub

--- Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.cpp
@@ -7,14 +7,35 @@ class Solution {
   public:
    int mod = 1e9+7;
     
-    int f(int ind, int target, vector<int> &arr, vector<vector<int>> &dp){
-        if(target < 0) return 0;
-        if(ind < 0) return target == 0;
+    // int f(int ind, int target, vector<int> &arr, vector<vector<int>> &dp){
+    //     if(target < 0) return 0;
+    //     if(ind < 0) return target == 0;
         
-        if(dp[ind][target] != -1) return dp[ind][target];
-        int pick = f(ind-1, target-arr[ind], arr, dp) % mod;
-        int notPick = f(ind-1, target, arr, dp) % mod;
-        return dp[ind][target] = (pick + notPick) % mod;
+    //     if(dp[ind][target] != -1) return dp[ind][target];
+    //     int pick = f(ind-1, target-arr[ind], arr, dp) % mod;
+    //     int notPick = f(ind-1, target, arr, dp) % mod;
+    //     return dp[ind][target] = (pick + notPick) % mod;
+    // }
+      int fnCount(vector<int>&arr,int index,int n,int d,int sum,vector<vector<int>> &dp)
+    {
+       
+       
+        if(sum<0)
+        return 0;
+        if(index==n)
+        {
+            if(sum==0)
+            return 1;
+            return 0;
+            
+        }
+        
+        if(dp[index][sum]!=-1)
+        return dp[index][sum];
+        
+        int val1=fnCount(arr,index+1,n,d,sum-arr[index],dp)%mod;
+        int val2=fnCount(arr,index+1,n,d,sum,dp)%mod;
+        return dp[index][sum]=(val1+val2)%mod;
     }
 
     int countPartitions(int n, int d, vector<int>& arr) {
@@ -26,7 +47,7 @@ class Solution {
         
         int target = ((d+totalSum)/2) % mod;
         vector<vector<int>> dp(n, vector<int>(target+1,-1));
-        return f(n-1, target, arr, dp);
+         return fnCount(arr,0,n,d,target,dp);
     }
 };
 
