1d8ef99edb98326bebca323d61b3d8a80aa43a6d
iam-hk <76506833+iam-hk@users.noreply.github.com>
2023-10-12 22:51:31 +0530

    Added solution - LeetHub

--- Word Break (Trie) - GFG/word-break-trie.java
+++ Word Break (Trie) - GFG/word-break-trie.java
@@ -33,94 +33,38 @@ public class GfG
 
 class Solution
 {
-    public static class Node{
-        boolean isEnd;
-        Node childs[];
-        Node()
-        {
-            isEnd=false;
-            childs=new Node[26];
-        }
-        
-    }
-    public static boolean fnFind(Node root,String s)
+    ////Recursive solution
+  
+   public static boolean fnFind(HashSet<String>hs,String a)
     {
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            if(root.childs[ch-'a']==null)
-            return false;
-            root=root.childs[ch-'a'];
-            
-        }
-        return root.isEnd;
-    }
-    public static boolean fnCheck(Node root,String s)
-    {
-        if(s.length()==0)
+        if(a.length()==0)
         return true;
-        for(int i=0;i<s.length();i++)
+        
+        for(int i=0;i<a.length();i++)
         {
-            String tmp=s.substring(0,i+1);
-            if(fnFind(root,tmp))
+            String tmp=a.substring(0,i+1);
+            if(hs.contains(tmp))
             {
-                boolean ans=fnCheck(root,s.substring(i+1));
-                if(ans==true)
+                boolean k=fnFind(hs,a.substring(i+1));
+                if(k==true)
                 return true;
             }
+            
         }
         return false;
     }
-    static Node root;
-    public static void fnInsert(Node root,String s)
-    {
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            if(root.childs[ch-'a']==null)
-            root.childs[ch-'a']=new Node();
-            root=root.childs[ch-'a'];
-            
-        }
-        root.isEnd=true;
-    }
-    // public static boolean fnFind(HashSet<String>hs,String a)
-    // {
-    //     if(a.length()==0)
-    //     return true;
-        
-    //     for(int i=0;i<a.length();i++)
-    //     {
-    //         String tmp=a.substring(0,i+1);
-    //         if(hs.contains(tmp))
-    //         {
-    //             boolean k=fnFind(hs,a.substring(i+1));
-    //             if(k==true)
-    //             return true;
-    //         }
-            
-    //     }
-    //     return false;
-    // }
+
     public static int wordBreak(String A, ArrayList<String> B )
     {
         //code here
-        root=new Node();
-      for(String s:B)
-      {
-          fnInsert(root,s);
-      }
-     // return fnCheck(root,A);
-        // HashSet<String>hs=new HashSet<>();
-        // for(String s:B)
-        // hs.add(s);
-        // if(fnFind(hs,A))
-        // return 1;
-        // return 0;
-        boolean val=fnCheck(root,A);
-        if(val)
+    
+        HashSet<String>hs=new HashSet<>();
+        for(String s:B)
+        hs.add(s);
+        if(fnFind(hs,A))
         return 1;
         return 0;
     }
 }
 
+  
